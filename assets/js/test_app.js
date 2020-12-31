cf91c259610930ff140b88917c2feab4ae1570ce
Saman Daneshvar <23390018+SamanDaneshvar@users.noreply.github.com>
2020-12-31 00:57:05 -0500

    Update test_app.js

--- assets/js/test_app.js
+++ assets/js/test_app.js
@@ -2,8 +2,13 @@ const firestore = firebase.firestore();
 const certificates_ref = firestore.collection("certificates");
 
 const output_header = document.querySelector("#heading");
-const input_certificate_number = document.querySelector("#certificate_number");
+const input_certificate_number = document.querySelector("#query_certificate");
 const get_button = document.querySelector("#get_button");
+
+const display_first_name = document.querySelector("#first_name");
+const display_last_name = document.querySelector("#last_name");
+const display_certificate_number = document.querySelector("#certificate_number");
+// ...
 
 get_button.addEventListener("click", function() {
   console.log("Getting the data from Firestore.");
@@ -19,3 +24,7 @@ get_button.addEventListener("click", function() {
       console.log("Error getting documents:", error);
     });
 })
+
+display_first_name.innerHTML = doc.get("first_name")
+display_last_name.innerHTML = doc.get("last_name")
+display_certificate_number.innerHTML = doc.get("certificate_number")
