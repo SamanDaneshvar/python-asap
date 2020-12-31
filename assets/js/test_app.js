212e3d0fbc5b4790097e8a8ccbe19f09aa5af5ff
Saman Daneshvar <23390018+SamanDaneshvar@users.noreply.github.com>
2020-12-30 23:24:27 -0500

    Update test_app.js

--- assets/js/test_app.js
+++ assets/js/test_app.js
@@ -1,35 +1,22 @@
-var firestore = firebase.firestore();
-const coll_ref = firestore.collection("certificates");
+const firestore = firebase.firestore();
+const collection_ref = firestore.collection("certificates");
 
 const output_header = document.querySelector("#heading");
 const input_text_field = document.querySelector("#certificate_number");
-const add_button = document.querySelector("#add_button");
 const get_button = document.querySelector("#get_button");
-
-add_button.addEventListener("click", function() {
-  const text_to_send = input_text_field.value;
-  console.log("Adding " + text_to_send + " to Firestore.");
-  coll_ref.add({
-    "certificate number": text_to_send
-  }).then(function() {
-    console.log("Certificate number saved.");
-  }).catch(function(error) {
-    console.log("Got an error: ", error);
-  });
-})
 
 get_button.addEventListener("click", function() {
   console.log("Getting the data from Firestore.");
-  coll_ref.get().then(function(doc) {
-    if (doc && doc.exists) {
-      const my_data = doc.data();
-      output_header.innerText = "The certificate number: " + doc.get("certificate number")
+  collection_ref.get().then(function(doc) {
+    if (doc.exists) {
+      console.log("Document data:", doc.data());
+      output_header.innerText = "Certificate number found: " + doc.get("certificate_number");
     } else {
-      console.log("It seems like the document doesn't exist!")
+      console.log("The document does not exist!")
       console.log("doc: ", doc)
-      console.log("doc.data: ", doc.data())
+      output_header.innerText = "No such certificate number found."
     }
   }).catch(function(error) {
-    console.log("Got an error: ", error);
+    console.log("Error getting document: ", error);
   });
 })
