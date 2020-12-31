5b8e294e1b5e4aa5006c9418ce74ab6f3a261e35
Saman Daneshvar <23390018+SamanDaneshvar@users.noreply.github.com>
2020-12-30 19:00:36 -0500

    Create init-firebase.js

---
+++ assets/js/init-firebase.js
@@ -0,0 +1,14 @@
+// The app's Firebase project configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCZ_QSTi7a41zQa4wtO-c53tpK1PVnFxYM",
+  authDomain: "python-certificates.firebaseapp.com",
+  projectId: "python-certificates",
+  storageBucket: "python-certificates.appspot.com",
+  messagingSenderId: "337313845571",
+  appId: "1:337313845571:web:c84ee85fa623a098f81ff6",
+  measurementId: "G-Z6WM0SCQ0T"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
