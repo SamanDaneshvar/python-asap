00be360f3d9d2160d0461711f7f92eee2d8f808e
Saman Daneshvar <23390018+SamanDaneshvar@users.noreply.github.com>
2020-12-30 22:24:25 -0500

    Update test_app.js

--- assets/js/test_app.js
+++ assets/js/test_app.js
@@ -3,15 +3,27 @@ const coll_ref = firestore.collection("certificates");
 
 const output_header = document.querySelector("#heading");
 const input_text_field = document.querySelector("#certificate_number");
-const send_button = document.querySelector("#send_button");
+const add_button = document.querySelector("#add_button");
+const get_button = document.querySelector("#get_button");
 
-send_button.addEventListener("click", function() {
+add_button.addEventListener("click", function() {
   const text_to_send = input_text_field.value;
   console.log("Sending " + text_to_send + " to Firestore.");
   coll_ref.add({
     "certificate number": text_to_send
   }).then(function() {
     console.log("Certificate number saved.");
+  }).catch(function(error) {
+    console.log("Got an error: ", error);
+  });
+})
+
+get_button.addEventListener("click", function() {
+  coll_ref.get().then(function(doc) {
+    if (doc && doc.exists) {
+      const my_data = doc.data();
+      output_header.innerText = "The certificate number: " + my_data."certificate number"
+    }
   }).catch(function(error) {
     console.log("Got an error: ", error);
   });
