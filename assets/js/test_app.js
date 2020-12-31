39f2d823a3d4abd369b6a1398f3874408babf25f
Saman Daneshvar <23390018+SamanDaneshvar@users.noreply.github.com>
2020-12-30 21:52:05 -0500

    Update test_app.js

--- assets/js/test_app.js
+++ assets/js/test_app.js
@@ -1,1 +1,18 @@
+var firestore = firebase.firestore();
+const doc_ref = firestore.doc("certificates");
 
+const output_header = document.querySelector("#heading");
+const input_text_field = document.querySelector("#certificate_number");
+const send_button = document.querySelector("#send_button");
+
+send_button.addEventListener("click", function() {
+  const text_to_send = input_text_field.value;
+  console.log("Sending " + text_to_send + " to Firestore.");
+  doc_ref.set({
+    "certificate number": text_to_send
+  }).then(function() {
+    console.log("Certificate number saved.");
+  }).catch(function(error) {
+    console.log("Got an error: ", error);
+  });
+})
