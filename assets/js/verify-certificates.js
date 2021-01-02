0a50cf3251f8796ec808eeddbb2fc037d2035316
Saman Daneshvar <23390018+SamanDaneshvar@users.noreply.github.com>
2021-01-02 14:20:05 -0500

    Update verify-certificates.js

--- assets/js/verify-certificates.js
+++ assets/js/verify-certificates.js
@@ -102,31 +102,6 @@ SEARCH_BY_NAME_BUTTON.addEventListener("click", async function() {
   let certificate_number;
   certificate_number = "2012-0486";
   console.log("Adding an event listener for certificate number:", certificate_number);
-  // Add an event listener on the hyperlink
-  let this_hyperlink = document.querySelector("#display_certificate_" + certificate_number.replace(/-/g, ""));
-  console.log("  Adding to:", this_hyperlink, this_hyperlink.innerHTML);
-  this_hyperlink.innerHTML += " -> Adding";
-  
-  this_hyperlink.addEventListener("click", function() {link_clicked();})
-    .catch(err => console.error("Error adding an event listener:", err));
-  
-  console.log("Successfully added the event listener.");
-  this_hyperlink.innerHTML += " -> Added";
-
-
-  certificate_number = "abc";
-  console.log("Adding an event listener for certificate number:", certificate_number);
-  // Add an event listener on the hyperlink
-  let this_hyperlink = document.querySelector("#display_certificate_" + certificate_number.replace(/-/g, ""));
-  console.log("  Adding to:", this_hyperlink, this_hyperlink.innerHTML);
-  this_hyperlink.innerHTML += " -> Adding";
-  
-  this_hyperlink.addEventListener("click", function() {link_clicked();})
-    .catch(err => console.error("Error adding an event listener:", err));
-  
-  console.log("Successfully added the event listener.");
-  this_hyperlink.innerHTML += " -> Added";
-  // }
 })
   .catch(err => console.error("Error adding an event listener to the search by name button:", err));
 
