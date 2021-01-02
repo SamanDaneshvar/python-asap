83b1eb5ba74f4d26cee822646975c21c22d05fdf
Saman Daneshvar <23390018+SamanDaneshvar@users.noreply.github.com>
2021-01-02 01:17:48 -0500

    Update verify-certificates.js

--- assets/js/verify-certificates.js
+++ assets/js/verify-certificates.js
@@ -91,11 +91,11 @@ SEARCH_BY_NAME_BUTTON.addEventListener("click", function() {
 		  // Add the hyperlink to the HTML
 		  LIST_OF_CERTIFICATES.innerHTML += hyperlink
 		  // Add an event listener on the hyperlink
-		  var temp = document.querySelector("#display_certificate_" + certificate_number.replace("-", ""))
+		  var temp = document.querySelector("#display_certificate_" + certificate_number.replace("-", ""));
 		  console.log("Selected link:", temp);
 		  temp.addEventListener("click", function() {
             console.log("The display certificate hyperlink has been clicked.");
-		    console.log("  for:", certificate_number);
+		    // console.log("  for:", certificate_number);
 		  })
 		  .catch(err => console.error(err));
 		}
