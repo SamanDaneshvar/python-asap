1161843bb60bdc38fe27960cc2ffeaac85c7a339
Saman Daneshvar <23390018+SamanDaneshvar@users.noreply.github.com>
2021-01-02 20:43:10 -0500

    Add the for loop

--- assets/js/verify-certificates.js
+++ assets/js/verify-certificates.js
@@ -76,35 +76,18 @@ async function button_clicked() {
 	});
   
   
-  // for (const certificate_number of ["2012-0486", "abc"]) {
-  let certificate_number;
-  let this_hyperlink;
-  
-  certificate_number = "2012-0486";
-  console.log("Adding an event listener for certificate number:", certificate_number);
-  // Add an event listener on the hyperlink
-  this_hyperlink = document.querySelector("#display_certificate_" + certificate_number.replace(/-/g, ""));
-  console.log("  Adding to:", this_hyperlink, this_hyperlink.innerHTML);
-  this_hyperlink.innerHTML += " -> Adding";
-  
-  this_hyperlink.addEventListener("click", link_clicked);
-  
-  console.log("Successfully added the event listener.");
-  this_hyperlink.innerHTML += " -> Added";
-
-
-  certificate_number = "abc";
-  console.log("Adding an event listener for certificate number:", certificate_number);
-  // Add an event listener on the hyperlink
-  this_hyperlink = document.querySelector("#display_certificate_" + certificate_number.replace(/-/g, ""));
-  console.log("  Adding to:", this_hyperlink, this_hyperlink.innerHTML);
-  this_hyperlink.innerHTML += " -> Adding";
-  
-  this_hyperlink.addEventListener("click", link_clicked);
-  
-  console.log("Successfully added the event listener.");
-  this_hyperlink.innerHTML += " -> Added";
-  // }
+  for (const certificate_number of ["2012-0486", "abc"]) {
+	  console.log("Adding an event listener for certificate number:", certificate_number);
+	  // Add an event listener on the hyperlink
+	  let this_hyperlink = document.querySelector("#display_certificate_" + certificate_number.replace(/-/g, ""));
+	  console.log("  Adding to:", this_hyperlink, this_hyperlink.innerHTML);
+	  this_hyperlink.innerHTML += " -> Adding";
+	  
+	  this_hyperlink.addEventListener("click", link_clicked);
+	  
+	  console.log("Successfully added the event listener.");
+	  this_hyperlink.innerHTML += " -> Added";
+  }
 }
 
 
